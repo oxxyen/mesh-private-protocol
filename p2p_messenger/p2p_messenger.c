81357928e7990c8a6c64dc0da22e8d385731ee93
JustCcode <k13775638@gmail.com>
2025-10-10 20:09:42 +0300

    updated all

--- p2p_messenger/p2p_messenger.c
+++ p2p_messenger/p2p_messenger.c
@@ -1,6 +1,5 @@
 #include "p2p_messenger.h"
 
-// === Signal Protocol Storage ===
 static int get_identity_key_pair(signal_buffer **public_data, signal_buffer **private_data, void *user_data) {
     p2p_network_t *net = (p2p_network_t*)user_data;
     ec_key_pair *key_pair;
@@ -17,18 +16,15 @@ static int get_local_registration_id(void *user_data, uint32_t *registration_id)
 }
 
 static int store_identity_key(const signal_protocol_address *address, uint8_t *key_data, size_t key_len, void *user_data) {
-    // Save to DB
     return 0;
 }
 
-// ... (остальные callback'и для Signal Protocol)
 
 int p2p_network_init(p2p_network_t *net, const char *username) {
     memset(net, 0, sizeof(*net));
     strncpy(net->local_username, username, MAX_USERNAME_LEN - 1);
     pthread_mutex_init(&net->mutex, NULL);
 
-    // Инициализация SQLite
     if (sqlite3_open(DB_PATH, &net->db) != SQLITE_OK) return -1;
 
     const char *sql = 
@@ -40,8 +36,6 @@ int p2p_network_init(p2p_network_t *net, const char *username) {
         "CREATE TABLE IF NOT EXISTS groups (name TEXT PRIMARY KEY, key BLOB);"
         "CREATE TABLE IF NOT EXISTS group_members (group_name TEXT, username TEXT);";
     sqlite3_exec(net->db, sql, NULL, NULL, NULL);
-
-    // Инициализация Signal Protocol
     signal_context_create(&net->global_context, NULL);
     signal_protocol_store_context_create(&net->store, net->global_context);
     
@@ -52,7 +46,6 @@ int p2p_network_init(p2p_network_t *net, const char *username) {
 
     key_helper_pre_key_list *pre_keys;
     key_helper_generate_pre_keys(&pre_keys, 1, 100, net->global_context);
-    // Сохраните pre-keys в store
 
     return 0;
 }
@@ -81,17 +74,15 @@ int encrypt_for_user(p2p_network_t *net, const char *to, const char *plaintext, 
 void *message_receiver(void *arg) {
     client_state_t *client = arg;
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    // ... bind, listen ...
 
     while (client->is_running) {
-        // accept + recv
+
         p2p_message_t msg;
         recv(..., &msg, sizeof(msg), 0);
 
         if (msg.type == 1) {
             char plaintext[MAX_MESSAGE_LEN];
             if (decrypt_from_user(&client->network, msg.from, msg.ciphertext, msg.ciphertext_len, plaintext) == 0) {
-                // Показать в chat_win
                 wprintw(client->chat_win, "\n%s: %s\n", msg.from, plaintext);
                 save_message(&client->network, msg.from, client->username, NULL, msg.ciphertext, msg.ciphertext_len, 0);
             }
